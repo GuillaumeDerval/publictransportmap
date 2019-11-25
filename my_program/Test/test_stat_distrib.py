a10d7d02924e445b9b6bf68c0909957701d691a3
Sophie <sophie.schorochoff@student.uclouvain.be>
2019-11-25 12:42:38 +0100

    Statistics method -tested

---
+++ my_program/Test/test_stat_distrib.py
@@ -0,0 +1,74 @@
+from unittest import TestCase
+from my_program.stat_distrib import *
+
+uniform_distrib = [0.25, 0.5, 0.75, 1]
+distrib = [0,0.1,0.2,0.2, 0.4, 0.5,1,1,1]
+
+
+class TestCheck_distrib(TestCase):
+    def test_check_distrib_good(self):
+        self.assertTrue(check_cumulative_distrib(uniform_distrib))
+        self.assertTrue(check_cumulative_distrib(distrib))
+        self.assertTrue(check_cumulative_distrib([0, 0, 1]))
+
+
+    def test_check_distrib_bad(self):
+        self.assertFalse(check_cumulative_distrib([]))
+        self.assertFalse(check_cumulative_distrib([-1, 0, 1]))
+        self.assertFalse(check_cumulative_distrib([0.3, 0.2, 0.4, 1]))
+        self.assertFalse(check_cumulative_distrib([0, 0.2, 0.4, 0.8]))
+
+
+
+class TestSum_distrib(TestCase):
+    def test_sum_distrib_no_change(self):
+        unit = [1]
+        self.assertEqual(sum_distrib(unit, unit), unit)
+        self.assertEqual(sum_distrib(uniform_distrib, unit), uniform_distrib)
+        self.assertEqual(sum_distrib(unit,distrib), distrib)
+    def test_sum_distrib_shift(self):
+        shift = [0,0,1]
+        self.assertEqual(sum_distrib(shift, shift), [0,0,0,0,1])
+        self.assertEqual(sum_distrib(uniform_distrib, shift), [0, 0, 0.25, 0.5, 0.75,1])
+        self.assertEqual(sum_distrib(shift,distrib), [0,0,0,0.1,0.2,0.2, 0.33333, 0.5,1,1,1])
+
+    def test_sum_distrib(self):
+        sum = sum_distrib(uniform_distrib, uniform_distrib)
+        self.assertTrue(check_cumulative_distrib(sum))
+        self.assertEqual(sum, [1 / 16, 3 / 16, 6 / 16, 10 / 16, 13 / 16, 15 / 16, 1])
+
+
+class TestMin_distrib(TestCase):
+    def test_min_distrib_one(self):
+        self.assertEqual(min_distrib([uniform_distrib]), uniform_distrib)
+
+    def test_min_distrib_2Simple(self):  # test fail but answert close enough from the target results
+        min1 = min_distrib([uniform_distrib, [1,1,1,1]])
+        self.assertTrue(check_cumulative_distrib(min1))
+        self.assertEqual(min1, [1,1,1,1])
+
+        min2 = min_distrib([uniform_distrib, [0, 0, 0, 1]])
+        self.assertTrue(check_cumulative_distrib(min2))
+        self.assertEqual(min2, uniform_distrib)
+
+        min3 = min_distrib([distrib, [0,0,0, 0,0,0, 0,0, 1]])
+        self.assertTrue(check_cumulative_distrib(min3))
+        self.assertEqual(min3, distrib)
+
+    def test_min_distrib3(self):
+        min1 = min_distrib([uniform_distrib, uniform_distrib])
+        self.assertTrue(check_cumulative_distrib(min1))
+        # self.assertEqual(min1, todo)
+
+        min2 = min_distrib([uniform_distrib, distrib])
+        self.assertTrue(check_cumulative_distrib(min2))
+
+    def test_min_distrib3(self):
+        min1 = min_distrib([uniform_distrib, uniform_distrib, uniform_distrib,uniform_distrib,uniform_distrib])
+        self.assertTrue(check_cumulative_distrib(min1))
+
+        min2 = min_distrib([uniform_distrib + [1]*5, distrib, distrib])
+        self.assertTrue(check_cumulative_distrib(min2))
+
+
+
--- my_program/stat_distrib.py
+++ my_program/stat_distrib.py
@@ -1,8 +1,66 @@
+import numpy
 
-def sum_distrib(f1,  f2):
-    #todo
-    pass
+def sum_distrib(F1, F2):
+    """
+    f1 and f2 must have the same step size (ex: step in minute)
+    :param F1: first cumulative distrbution form : [prop_0_min, prop_1_min ,..., prop_max_time-1_min, prop_stop_unreached]
+    :param F2: second cumulative distribution distrbution form
+    :return: cumaltive distribution representing the (f1 + f2)
+    """
+    assert check_cumulative_distrib(F1) and check_cumulative_distrib(F2)
+    l1 = len(F1)
+    l2 = len(F2)
+    l_sum = l1 + l2 - 1
+    f_sum = [0] * (l_sum)
+
+    f1 = [F1[0]] + [F1[i] - F1[i-1] for i in range(1,l1)]
+    f2 = [F2[0]] + [F2[i] - F2[i - 1] for i in range(1, l2)]
+
+    for id1 in range(l1):
+        for id2 in range(l2):
+            f_sum[id1 + id2] += f1[id1] * f2[id2]
+
+    #To cumulative distribution
+    F_sum = [f_sum[0]] + [0] * (l_sum - 1)
+    for i in range(1, l_sum):
+        F_sum[i] = f_sum[i] + F_sum[i-1]
+    assert check_cumulative_distrib(F_sum)  # check for safety
+    return F_sum
 
 def min_distrib(list_Fi):
-    #todo
-    pass
+    """
+
+    :param list_Fi: the distribution must have the same len and step
+    :return:
+    """
+    for fi in list_Fi:
+        assert check_cumulative_distrib(fi)
+
+    #unfinished_list_Fi = list_Fi[:] # line to handle distribution wit
+    l = len(list_Fi[0])
+    F_min = [0] * l
+    for i in range(l):
+        # F_min <= x iff f0 <= x or f1 <= x or ...  or f(l-1) <= x
+
+        # F_min > x iff  not(f0 <= x or f1 <= x or ...  or f(l-1) <= x)
+        # F_min > x iff not(f0 <= x) and not(f1 <= x) and ...  and not(f(l-1) <= x))
+        # P(F_min > x) iff  P(f0 > x) * P(f1 > x) * ...  * P(f(l-1) > x))
+        #
+        # P(F_min <= x) = 1 - P(F_min > x)
+        # P(F_min <= x ) = 1 - [ (1-P(f0 <= x)) * (1-P(f1 <= x)) * ... *(1-P(f(l-1) <= x))]
+        prod = 1
+        for fi in list_Fi:
+            prod *= (1- fi[i])
+        F_min[i] = 1 - prod
+    assert check_cumulative_distrib(F_min) # check for safety
+    return F_min
+
+def check_cumulative_distrib(F):
+    """ return true if F is a increasing function with maximum = 1"""
+    if len(F) == 0: return False
+    for i in range(0, len(F)-1):
+        if not (0 <= F[i] <= 1 and F[i] <=  F[i+1]):
+            return False
+    if F[len(F)-1] != 1:
+        return False
+    return True
