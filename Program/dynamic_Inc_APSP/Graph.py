03dabf5fb289d07aac3af5435be3a0b92a768dfb
Sophie <sophie.schorochoff@student.uclouvain.be>
2020-04-20 18:42:54 +0200

    cleaning Graph

--- Program/dynamic_Inc_APSP/Graph.py
+++ Program/dynamic_Inc_APSP/Graph.py
@@ -1,32 +1,30 @@
 class Graph:
     """
-    Transforme le fichier out.json en une structure de graph et permet le modification ainsi que leur sauvegarde
-
-    Propriete de ce graph quand il existe un chemin entre 2 de ces noeud il est toujour minimal
-    Pas de chemin est indiqué par -1
+    Transform the file graph.json into a graph structure which can be modified
+    This class record also the modification done in order to undo them.
     """
     def __init__(self, out):
-        self.adj_matrix: dict = {int(k): value for k,value in out["graph"].items()}
-        self.vertex: list = [int(v) for v in self.adj_matrix.keys()]
-        self.V: int = len(self.adj_matrix)                                       # number of vertex
-        self.E: int = sum([len(nei) for _, nei in self.adj_matrix.items()])      # number of edges
+        self.adj_list: dict = {int(k): value for k, value in out["graph"].items()}   # adjacency lists for outgoing edge
+        self.vertex: list = [int(v) for v in self.adj_list.keys()]                   # list of vertex
+        self.V: int = len(self.adj_list)                                             # number of vertex
+        self.E: int = sum([len(nei) for _, nei in self.adj_list.items()])            # number of edges
 
-        # adjacency matrix with reversed edge (if u -> v in g the v->u in rev_g)
-        self.reversed_adj_matrix: dict = {v: [] for v in self.vertex}
+        # adjacency list with reversed edge (if u -> v in g the v->u in rev_g)
+        self.reversed_adj_list: dict = {v: [] for v in self.vertex}                  # adjacency lists for incoming edge
         for org in self.vertex:
-            for dest in self.adj_matrix[org]:
-                self.reversed_adj_matrix[dest].append(org)
+            for dest in self.adj_list[org]:
+                self.reversed_adj_list[dest].append(org)
 
-        self.vis = {v: False for v in self.vertex}          # indicate if a node have been visited
+        self.vis = {v: False for v in self.vertex}                                # indicate if a node have been visited
 
         # reversible state -> record change
         self.__change_log = []
-        self.__stack_log = []                               # permet de faire une recherche sur plusieur etage
+        self.__stack_log = []
 
     def add_vertex(self, v):
         if v not in self.vertex:
-            self.adj_matrix[v] = []
-            self.reversed_adj_matrix[v] = []
+            self.adj_list[v] = []
+            self.reversed_adj_list[v] = []
             self.vertex.append(v)
             self.V += 1
             self.vis[v] = False
@@ -37,21 +35,21 @@ class Graph:
         assert u in self.vertex
         assert v in self.vertex
 
-        if v not in self.adj_matrix[u]:                         # keep a simple graph
+        if v not in self.adj_list[u]:                         # keep the graph simple
             self.E += 1
-            self.adj_matrix[u].append(v)
-            self.reversed_adj_matrix[v].append(u)
+            self.adj_list[u].append(v)
+            self.reversed_adj_list[v].append(u)
             self.__change_log.append(("add_edge", u, v))
 
     def remove_vertex(self, z):
         assert z in self.vertex
 
-        for dest in self.adj_matrix[z]:
+        for dest in self.adj_list[z]:
             self.remove_edge(z, dest)
-        self.adj_matrix.pop(z)
-        for org in self.reversed_adj_matrix[z]:
+        self.adj_list.pop(z)
+        for org in self.reversed_adj_list[z]:
             self.remove_edge(org, z)
-        self.reversed_adj_matrix.pop(z)
+        self.reversed_adj_list.pop(z)
 
         self.vertex.remove(z)
         self.V -= 1
@@ -61,17 +59,17 @@ class Graph:
     def remove_edge(self, u, v):
         assert u in self.vertex
         assert v in self.vertex
-        assert v in self.adj_matrix[u]
+        assert v in self.adj_list[u]
         self.E -= 1
-        self.adj_matrix[u].remove(v)
-        self.reversed_adj_matrix[v].remove(u)
+        self.adj_list[u].remove(v)
+        self.reversed_adj_list[v].remove(u)
         self.__change_log.append(("rm_edge", u, v))
 
     def in_degree(self, v):
-        return len(self.reversed_adj_matrix[v])
+        return len(self.reversed_adj_list[v])
 
     def out_degree(self, v):
-        return len(self.adj_matrix[v])
+        return len(self.adj_list[v])
 
     def save(self):
         self.__stack_log.append(self.__change_log)
@@ -89,15 +87,5 @@ class Graph:
         self.__change_log = self.__stack_log.pop()
 
     def __eq__(self, other):
-        if isinstance(other, Graph):
-            if self.V == other.V and self.vertex == other.vertex:
-                if self.E == other.E and  self.adj_matrix == other.adj_matrix:
-                    return True
-                else:
-                    print("bad adj matrix")
-            else:
-                print("bad vertex matrix")
-        else:
-            print("bad instance")
-        return False
-        #return isinstance(other, Graph) and self.V == other.V and self.E == other.E and self.vertex == other.vertex and self.adj_matrix == other.adj_matrix
+        return isinstance(other, Graph) and self.V == other.V and self.E == other.E \
+               and self.vertex == other.vertex and self.adj_list == other.adj_list
