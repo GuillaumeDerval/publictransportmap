615c40e5275916206ec9b8307e5c979cdac9e0fd
Sophie <sophie.schorochoff@student.uclouvain.be>
2020-06-14 15:40:16 +0200

    Suppression de fichier et commit stop_lambert

--- stats.py
+++
@@ -1,5 +0,0 @@
-import json
-
-entries = json.load("produce/train_bus_simplified.json")
-print("NB STOPS", len(entries.keys()))
-print("NB CONNECTIONS", sum([len(entries[x]['nei']) for x in entries]))
